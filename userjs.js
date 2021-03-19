d619c678f3147108cc21b423e00c9d93920f8af9
IngSm <76782554+IngSm@users.noreply.github.com>
2021-03-19 10:39:29 +0700

    "Go" elements and working auth_form

--- userjs.js
+++ userjs.js
@@ -1,1 +1,13 @@
+let GoAuth = document.getElementById('ChInBtn').addEventListener('click', OpenAuth);
 
+function OpenAuth () {
+    document.location.href ="http://www.google.com"
+}
+
+document.forms.AuthForm.onsubmit = function () {
+    let Uname = this.email.value;
+    let Upassword = this.password.value;
+    localStorage.setItem (Uname, Upassword);
+    open('index.html');
+    return false;
+}
