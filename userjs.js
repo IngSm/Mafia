68cdcad5f4fbce3538f133c85bf7a6c8b17139d1
IngSm <76782554+IngSm@users.noreply.github.com>
2021-03-25 12:16:10 +0700

    Verification_Check_Added

--- userjs.js
+++ userjs.js
@@ -1,13 +1,16 @@
 let GoAuth = document.getElementById('ChInBtn').addEventListener('click', OpenAuth);
 
 function OpenAuth () {
-    document.location.href ="http://www.google.com"
+    document.location.href ="authform.html"
 }
+
+let Uname;
+let Upassword;
 
 document.forms.AuthForm.onsubmit = function () {
     let Uname = this.email.value;
     let Upassword = this.password.value;
     localStorage.setItem (Uname, Upassword);
-    open('index.html');
+    document.location.href = "index.html";
     return false;
 }
